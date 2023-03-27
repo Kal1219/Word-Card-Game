b5f9c8ccc122ade58d987426dbbaebec705146ef
Kal1219 <kaleb.cortes1208@gmail.com>
2023-03-27 13:24:37 -0600

    Initial Commit

---
+++ Word Card Game/Word_Card_GameApp.swift
@@ -0,0 +1,17 @@
+//
+//  Word_Card_GameApp.swift
+//  Word Card Game
+//
+//  Created by Kaleb on 27/03/23.
+//
+
+import SwiftUI
+
+@main
+struct Word_Card_GameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
