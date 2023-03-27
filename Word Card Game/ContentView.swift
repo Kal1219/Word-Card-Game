9508a44e672cf053909879983584c51fe26da233
Kal1219 <kaleb.cortes1208@gmail.com>
2023-03-27 14:04:24 -0600

    base visual de juego word card game

--- Word Card Game/ContentView.swift
+++ Word Card Game/ContentView.swift
@@ -7,20 +7,68 @@
 
 import SwiftUI
 
-struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+struct ContentView: View
+{
+    var body: some View
+    {
+        ZStack
+        {
+            Image("background-plain")
+                .ignoresSafeArea()
+            
+            VStack(alignment: .center, spacing: 40.0)
+            {
+                Image("logo")
+                
+                HStack
+                {
+                    Spacer()
+                    Spacer()
+                    Image("card2")
+                    Spacer()
+                    Image("card3")
+                    Spacer()
+                    Spacer()
+                }
+                
+                Image("button")
+                
+                HStack
+                {
+                    Spacer()
+                    VStack(spacing: 40.0)
+                    {
+                        Text("Player")
+                        Text("0")
+                            .font(.largeTitle)
+                    }
+                    .foregroundColor(.white)
+                    
+                    
+                    
+                    Spacer()
+                    
+                    VStack(spacing: 40.0)
+                    {
+                        Text("CPU")
+                        Text("0")
+                            .font(.largeTitle)
+                    }
+                    .foregroundColor(.white)
+                    
+                    
+                    
+                    Spacer()
+                }
+            }
         }
-        .padding()
     }
 }
 
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
+struct ContentView_Previews: PreviewProvider
+{
+    static var previews: some View
+    {
         ContentView()
     }
 }
